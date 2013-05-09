52a1fbc26baa3ea468075d0159b1a99b057b2ea6
Rain Lee <raincious@gmail.com>
2013-05-09 19:06:31 +0800

    Add and remove files that forgot to add or remove

---
+++ facula/routine/routine.coreinterfaces.php
@@ -0,0 +1,34 @@
+<?php
+
+interface coreInterface {
+	static public function getInstance(&$cfg, $commonCfg, facula $parent);
+	static public function checkInstance($instance);
+}
+
+// Yes, now i just need extends below core factory and everything will be just fine. 
+// And you can still replace faculaCore with your own core
+// I even starting wondering why i have so many cores in different 'core' that only for extends this class for running like a factory
+abstract class faculaCoreFactory implements coreInterface { 
+	static private $instances = array();
+	
+	static public final function getInstance(&$cfg, $commonCfg, facula $parent) {
+		$caller = get_called_class();
+		// If $cfg['Custom'] has beed set, means user wants to use their own core instead of default
+		$class = $caller . (isset($cfg['Core'][0]) ? $cfg['Core'] : 'Default');
+		
+		if (!isset(self::$instances[$class])) {
+			if (!class_exists($class)) {
+				throw new Exception('Facula core ' . $class . ' is not loadable. Please make sure object file has been included before preform this task.');
+			}
+			
+			// Create and check new instance
+			if ($caller::checkInstance(self::$instances[$class] = new $class($cfg, $commonCfg, $parent))) {
+				return self::$instances[$class];
+			}
+		}
+		
+		return self::$instances[$class];
+	}
+}
+
+?>
