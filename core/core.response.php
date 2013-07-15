42a2d3e85bd84f9ca67131e488da45fe6a8b60ba
raincious <raincious@gmail.com>
2013-07-15 15:13:46 +0800

    Remove comment for anit-misunderstanding

--- core/core.response.php
+++ core/core.response.php
@@ -320,7 +320,6 @@ class faculaResponseDefault implements faculaResponseInterface {
 			echo self::$content;
 			
 			echo ob_get_flush();
-			// Belowing flush both needed.
 			flush();
 			
 			return true;
