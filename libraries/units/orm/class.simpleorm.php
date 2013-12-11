9cd9c9b18284d47b53ae2da8687d070af9fe82d6
Rain Lee <raincious@gmail.com>
2013-12-11 20:36:47 +0800

    Yes, you can now, set noParser to false.

--- libraries/units/orm/class.simpleorm.php
+++ libraries/units/orm/class.simpleorm.php
@@ -139,7 +139,7 @@ abstract class SimpleORM implements ormInterface, ArrayAccess {
 		
 		$query = null;
 		
-		$query = query::from($this->table, $this->noParser);
+		$query = query::from($this->table, !$this->noParser);
 		$query->select($this->fields);
 		
 		if (isset($param['Where'])) {
@@ -391,7 +391,7 @@ abstract class SimpleORM implements ormInterface, ArrayAccess {
 				}
 			}
 			
-			if ($result = query::from($this->table, $this->noParser)->update($this->fields)->set($data)->where('AND', $this->primary, '=', $primaryKey)->save()) {
+			if ($result = query::from($this->table, !$this->noParser)->update($this->fields)->set($data)->where('AND', $this->primary, '=', $primaryKey)->save()) {
 				$this->dataOriginal = $this->data;
 				
 				return $result;
@@ -415,7 +415,7 @@ abstract class SimpleORM implements ormInterface, ArrayAccess {
 		}
 		
 		// Must returning primary key
-		if ($result = query::from($this->table, $this->noParser)->insert($keys)->value($data)->save($this->primary)) {
+		if ($result = query::from($this->table, !$this->noParser)->insert($keys)->value($data)->save($this->primary)) {
 			$this->dataOriginal = $this->data;
 			
 			if (!isset($this->data[$this->primary])) {
@@ -432,7 +432,7 @@ abstract class SimpleORM implements ormInterface, ArrayAccess {
 		$result = null;
 		
 		if (isset($this->data[$this->primary])) {
-			if ($result = query::from($this->table, $this->noParser)->delete($this->fields)->where('AND', $this->primary, '=', $this->data[$this->primary])->save()) {
+			if ($result = query::from($this->table, !$this->noParser)->delete($this->fields)->where('AND', $this->primary, '=', $this->data[$this->primary])->save()) {
 				$this->dataOriginal = $this->data = array();
 				
 				return $result;
