f9cd64bc850db0f61a401251ad986402841958e1
raincious <raincious@gmail.com>
2014-04-02 20:17:35 +0800

    Update compiler interface.

--- src/Facula/Base/Implement/Core/Template/Compiler.php
+++ src/Facula/Base/Implement/Core/Template/Compiler.php
@@ -32,8 +32,6 @@ namespace Facula\Base\Implement\Core\Template;
  */
 interface Compiler
 {
-    public static function addTag($tag, $command);
-
     public function __construct(array &$pool, &$sourceTpl);
     public function compile();
 }
